4d49352fca71f6f74ebee2c49b8c176b647a6a32
StJohn Piano <stjohnpiano@gmail.com>
2023-10-05 08:57:37 +0100

    Exclude headers from request logging

--- src/middlewares/log-requests.ts
+++ src/middlewares/log-requests.ts
@@ -1,14 +1,16 @@
 import audit from 'express-requests-logger';
-import { createLogger } from '../../lib/logging';
+import { createLogger } from '#lib/logging';
 
 const { logger, log, deb } = createLogger({fileName: __filename});
 
 export const logRequestsMiddleware = audit({
   logger: logger,
   request: {
+      excludeHeaders: ['*'],
       maxBodyLength: 50, // limit length to 50 chars + '...'
   },
   response: {
+      excludeHeaders: ['*'],
       maxBodyLength: 50, // limit length to 50 chars + '...'
   },
 });
