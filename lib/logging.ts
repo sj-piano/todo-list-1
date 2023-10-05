1392f31a0fbae391226f3df37eea4fd11e4a5d9b
StJohn Piano <stjohnpiano@gmail.com>
2023-10-05 14:06:37 +0100

    Add function name and line number to log messages

--- lib/logging.ts
+++ lib/logging.ts
@@ -1,6 +1,9 @@
+// Imports
 import winston from "winston";
 import DailyRotateFile = require("winston-daily-rotate-file");
 
+
+// Validate log level
 enum LogLevelList {
   "error",
   "warn",
@@ -9,12 +12,85 @@ enum LogLevelList {
 }
 type LogLevel = keyof typeof LogLevelList;
 
+
 function getLogLevel(logLevel: string): LogLevel {
   if (logLevel in LogLevelList) {
     return logLevel as LogLevel;
   }
   throw new Error(`Invalid log level: ${logLevel}`);
 }
+
+
+function isString(myVar: any): boolean {
+  return typeof myVar === 'string' || myVar instanceof String;
+}
+
+
+function isObject(myVar: any): boolean {
+  return typeof myVar === 'object' && myVar !== null;
+}
+
+
+let log2 = console.log;
+
+
+interface StackTraceLine {
+  functionName: string;
+  filePath: string;
+  lineNumber: number;
+  columnNumber: number;
+}
+
+
+function parseStackTraceLine(stackTraceLine: string): StackTraceLine {
+  // https://github.com/felixge/node-stack-trace/blob/master/index.js
+  /* Example lines:
+  at captureFunctionAndLine (/app/lib/logging.ts:33:19)
+  at Logger.info (/app/lib/logging.ts:167:20)
+  at processTicksAndRejections (node:internal/process/task_queues:95:5)
+  at Object.onceWrapper (node:events:628:28)
+  */
+ // Replace multiple spaces with a single space
+  stackTraceLine = stackTraceLine.replace(/\s+/g, ' ');
+  let sections = stackTraceLine.trim().split(" ");
+  let functionName = sections[1];
+  let location = sections[2];
+  location = location.slice(1, -1); // Remove parentheses
+  let items = location.split(":");
+  let filePath = items.slice(0, -2).join(":"); // Collect all but the last two items
+  let lineNumber = items[items.length - 2];
+  let columnNumber = items[items.length - 1];
+  let result = {
+    functionName,
+    filePath,
+    lineNumber: parseInt(lineNumber, 10),
+    columnNumber: parseInt(columnNumber, 10),
+  };
+  return result;
+}
+
+
+function captureFunctionAndLine(index: number = 2) {
+  try {
+    throw new Error();
+  } catch (e) {
+    let stack = e.stack;
+    let lines = stack.split("\n").slice(1);
+    // Use index = 2 to move up through:
+    // - captureFunctionAndLine
+    // - Logger.info
+    // and arrive at the caller.
+    // Logger.log needs to pass in index = 3 so that we move up an extra level.
+    if (index > lines.length) {
+      index = lines.length - 1;
+    }
+    let line = lines[index];
+    let result = parseStackTraceLine(line);
+    return `[${result.functionName} : ${result.lineNumber}]`;
+  }
+}
+
+
 
 class Logger {
   logger: winston.Logger;
@@ -35,6 +111,7 @@ class Logger {
     }
     const logFormatterConsole = (metadata: any) => {
       let { level, message, stack, timestamp } = metadata;
+      //log2(metadata);
       // An error object will have a stack property, and we'll use this instead of the message.
       message = stack || message;
       if (metadata.messageOnly) {
@@ -49,10 +126,15 @@ class Logger {
         message = message.trimStart(); // Remove effect of align().
         return message;
       }
-      let s = `${level}: ${message}`;
+      // Add spaces to align log levels.
+      const m = 8;
+      const spacing = level.length < m ? m - level.length : 0;
+      const spaces = " ".repeat(spacing);
+      let s = `${level}${spaces}: `;
       if (fileName) {
-        s += ` - ${fileName}`;
+        s += `${fileName} `;
       }
+      s += `${message}`;
       if (logTimestamp) {
         s = `${timestamp} ` + s;
       }
@@ -65,7 +147,6 @@ class Logger {
           winston.format.timestamp({
             format: "YYYY-MM-DD HH:mm:ss.SSS",
           }),
-          winston.format.align(),
           winston.format.printf(logFormatterConsole),
         ),
       }),
@@ -119,44 +200,64 @@ class Logger {
     }
   }
 
-  debug(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.logger.debug(arg);
+  debug(message: any, meta?: { index: number}) {
+    let index = 2;
+    if (meta) {
+      index = meta.index;
+    }
+    let location = captureFunctionAndLine(index);
+    if (! isString(message)) {
+      message = JSON.stringify(message, null, 2);
+    }
+    message = `${location} ${message}`;
+    this.logger.debug(message);
   }
 
-  info(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.logger.info(arg);
+  info(message: any, meta?: { index: number}) {
+    let index = 2;
+    if (meta) {
+      index = meta.index;
+    }
+    let location = captureFunctionAndLine(index);
+    if (! isString(message)) {
+      message = JSON.stringify(message, null, 2);
+    }
+    message = `${location} ${message}`;
+    this.logger.info(message);
   }
 
-  warn(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.logger.warn(arg);
+  warn(message: any) {
+    this.logger.warn(message);
   }
 
-  error(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.logger.error(arg);
+  error(message: any) {
+    this.logger.error(message);
   }
 
-  deb(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.debug(arg);
+  deb(message: any, meta?: { index: number}) {
+    if (! meta) {
+      meta = { index: 3 };
+    }
+    this.debug(message, meta);
   }
 
-  dj(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.deb(JSON.stringify(arg, null, 2));
+  dj(message: any) {
+    let meta = { index: 4 };
+    message = JSON.stringify(message, null, 2);
+    this.deb(message, meta);
   }
 
-  log(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.info(arg);
+  log(message: any, meta?: { index: number}) {
+    if (! meta) {
+      meta = { index: 3 };
+    }
+    this.info(message, meta);
   }
 
-  lj(...args: any[]) {
-    let arg = args.length == 1 ? args[0] : args;
-    this.log(JSON.stringify(arg, null, 2));
+  lj(message: any) {
+    let meta = { index: 4 };
+    message = JSON.stringify(message, null, 2);
+    this.log(message, meta);
   }
 
 }
